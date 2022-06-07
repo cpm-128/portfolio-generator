af1aedb6f5d6d53bdb34fea61cc4e587b7e630b1
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-07 12:28:29 -0400

    interpolated colland-line arguments into the html

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+    //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
+const [name, github] = profileDataArgs;
 
-    // THIS....
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = (name, github) => {
+    return `
 
-    console.log('=========')
+    <!DOCTYPE html>
+    <html lang="en">
 
-    // IS THE SAME AS THIS....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">GitHub</a></h2>
+    </body>
+
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name , github));
+
+// const printProfileData = profileDataArr => {
+
+//     // THIS....
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('=========')
+
+//     // IS THE SAME AS THIS....
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
