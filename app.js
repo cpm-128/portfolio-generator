19fe8340e3e54c2193c4e4f0de21cd941a2d3f28
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-08 14:21:38 -0400

    gather info for a single project

--- app.js
+++ app.js
@@ -8,15 +8,77 @@ const inquirer = require('inquirer');
 // const generatePage = require('./src/page-template.js');
 
 // inquirer questions and answers
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself.'
+        }
+
+    ])
+};
+
+const promptProject = () => {
+    // backticks allow for carriage returns
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all the apply)',
+            choices: ['JavaScript' , 'HTML' , 'CSS' , 'ES6' , 'jQuery' , 'Bootstrap' , 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(">>> " , answers));
+};
+
+// use .then functions to control the application's flow
+promptUser()
+    .then(answers => console.log(">>> about user" , answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(">>> about project" , projectAnswers));
+    // how did projectAnswers get defined here??
 
 // // populate html text
 // const pageHTML = generatePage(name, github);
