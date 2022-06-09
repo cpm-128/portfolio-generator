5da876cabdb7199461f8e560802acd47c1ff9c6f
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-09 13:18:18 -0400

    project html section

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // add inquirer module
 const inquirer = require('inquirer');
 
-// // add file system library
-// const fs = require('fs');
+// add file system library
+const fs = require('fs');
 
-// // receive the exported function
-// const generatePage = require('./src/page-template.js');
+// receive the exported function
+const generatePage = require('./src/page-template.js');
 
 // inquirer questions and answers
 const promptUser = () => {
@@ -148,34 +148,14 @@ promptUser()
     //.then(answers => console.log(">>> about user" , answers))
     .then(promptProject)
     .then(portfolioData => {
-        console.log(">>> portfolio data" , portfolioData)
+        //console.log(">>> portfolio data" , portfolioData)
+
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./indexhtml' , pageHTML , err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio created. Check out index.html in this directory to see it.');
+        });
     });
     // how did projectAnswers get defined here??
-
-// // populate html text
-// const pageHTML = generatePage(name, github);
-
-// // use module to actually generate an html file
-// // fs.writefile ( file name , data being written , callback function for errors/success )
-// fs.writeFile('./index.html' , pageHTML , err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete. Check out index.html to see the output.');
-// });
-
-// =========
-
-// const printProfileData = profileDataArr => {
-
-//     // THIS....
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('=========')
-
-//     // IS THE SAME AS THIS....
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
